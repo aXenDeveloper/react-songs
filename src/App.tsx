227f652d68d5d31918efa51e0d0360de21f3c3e4
aXenDev <58148176+aXenDeveloper@users.noreply.github.com>
2020-11-07 17:15:07 +0100

    Added styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import Player from './components/Player';
+import Song from './components/Song';
 
 const App = () => {
-	return <div>test</div>;
+	return (
+		<div className='children'>
+			<Song />
+			<Player />
+		</div>
+	);
 };
 
 export default App;
---
+++ src/components/Song.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Song = () => {
+    return (
+        <div className="song-container">
+            <h1>Picture</h1>
+            <p>Song name</p>
+            <p>Artist</p>
+        </div>
+    )
+};
+
+export default Song;
