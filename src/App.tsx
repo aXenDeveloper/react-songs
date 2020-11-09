64c37eea7affeb66056804d19e753c8ae0b61c09
aXenDev <58148176+aXenDeveloper@users.noreply.github.com>
2020-11-09 18:49:05 +0100

    Added assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Player from './components/Player';
 import Song from './components/Song';
+import data from './data';
 
 const App = () => {
+	const [song, setSong] = useState(data());
+
 	return (
 		<div className='children'>
 			<Song />
